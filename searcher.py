8225e59fc1e107e5af0fede9d840aeab228e825a
ngrande <devlup@outlook.com>
2016-05-31 17:30:28 +0200

    working on large-file support

--- searcher.py
+++ searcher.py
@@ -26,22 +26,15 @@ class Searcher:
             temp_file_path = directory + '/' + file_name
             if os.path.isdir(temp_file_path):
                 # if file is a directory -> recursive call to itself
-
-                # ATTENTION not yet threaded! will wait and the end of it's
-                # call for the results
                 self.search_for_pattern_async(temp_file_path, pattern)
             else:
                 self._start_search_thread(temp_file_path, pattern)
 
-        # return self._wait_for_results()
-
     def _search_callback(self, result):
-        # print('search callback invoked')
         self.thread_count -= 1
         self.all_results.extend(result)
 
     def _start_search_thread(self, file_path, pattern):
-        # print('starting search thread')
         thread = SearchThread(self.executor)
         self.thread_count += 1
         thread.search_in_file_async(file_path, pattern, self._search_callback)
@@ -65,8 +58,9 @@ class SearchThread:
         self.search_thread = None
         self.search_callback = None
         self.threadpool_exec = threadpool_exec
+        self.max_file_junk_size = 5242880  # bytes => 5 MiB
 
-    def _search_in_file(self, file_path, pattern):
+    def _search_in_file(self, file_path, pattern, start_pos, end_pos):
         temp_result = []
         for line in open(file_path, mode='r', errors='replace'):
             match = re.search(pattern, line)
@@ -74,14 +68,23 @@ class SearchThread:
                 temp_result.append(line)
 
         if self.search_callback is not None:
-            # print('file search finished - invoking callback')
             self.search_callback(temp_result)
 
     def search_in_file_async(self, file_path, pattern, search_callback):
         self.search_callback = search_callback
-        # self.search_thread = threading.Thread(target=self._search_in_file,
-        #                                      args=(file_path, pattern))
-        # self.search_thread.start()
         self.threadpool_exec.submit(self._search_in_file, file_path, pattern)
-        # print('searchThread started')
-        # return self.search_thread
+
+    def _calc_positions(self, file_path):
+        size = os.path.getsize(file_path)
+        if size >= self.max_file_junk_size:
+            div = int(size / self.max_file_junk_size)  # how many times the max_file_junk_size fits into the file size
+            rest = size % self.max_file_junk_size  # the rest which does not fit anymore
+            results = []
+            for i in range(0, div):
+                temp_pos = i * self.max_file_junk_size
+                # add a -1?
+                results.append((temp_pos, temp_pos + self.max_file_junk_size))
+            # add the rest
+            last = results[len(results) - 1]
+            results.append((last, last+rest))
+            # not done yet...
