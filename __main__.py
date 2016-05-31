85b874009a115e685e248178489896b57071c07b
ngrande <devlup@outlook.com>
2016-05-31 16:00:21 +0200

    added gif animation

--- __main__.py
+++ __main__.py
@@ -9,21 +9,18 @@ args_parser = argparse.ArgumentParser(description='Search all files in a '
                                       'directory (including subdirectories) '
                                       'for a regex pattern')
 args_parser.add_argument('-p', '--regex-pattern', help='regex pattern to '
-                         'search for in the files in the directory',
+                         'search for in the file(s) in the directory',
                          required=True)
-args_parser.add_argument('-o', '--output-file', nargs='?', help='output file '
-                         'with matching lines (will be created after script '
-                         'finished)', default='matches.txt', type=str)
+args_parser.add_argument('-o', '--output-file', nargs='?', help='path to the '
+                         'output file where all matching lines will be written'
+                         ' to', default='matches.txt', type=str)
 args_parser.add_argument('-d', '--directory', nargs='?', type=str,
-                         help='directory of files which will be scanned',
+                         help='directory of file(s) which will be scanned',
                          default='./')
 args_parser.add_argument('-n', '--number-of-threads', nargs='?', type=int,
                          help='maximum number of concurrent threads used to '
                          'scan the files', default=10)
-# args_parser.add_argument('-l', '--deactivate-live-output', help='This '
-#  'argument turns off the live output'
-#  'of a file in the console', action='store_false',
-#  default=True)
+
 args = args_parser.parse_args()
 searcher = Searcher(args.number_of_threads)
 start_time = time.time()
@@ -35,6 +32,7 @@ print('time elapsed:')
 utils.print_elapsed_time(start_time=start_time, end_time=end_time)
 print('#### found {0!s} matching line(s) ####'.format(len(matching_lines)))
 if len(matching_lines) > 0:
+    print('writing matching lines into output file... (may take a second)')
     with open(args.output_file, 'w+') as file:
         file.writelines(matching_lines)
 print('done!')
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Sing
 acronym for **S**ing **i**s **n**ot **g**ood
 
-Sing is an easy to use search tool. You can search all files in a directory and its subdirectories using regular expressions.
+Sing is an easy to use search tool. You can search all files in a directory and its subdirectories (or just a single file) using regular expressions and save the matching lines in an output file. Useful if you are looking for error messages in many log files.
 
-__BUT__: It is _slow_ as it is _simple_. Not recommend to use for fast searching.
+__BUT:__ It is _slow_ as it is _simple_. Not recommend to use for fast searching.
+It needs ~__1.9 Minutes__ to scan __3.5 GB__ of files (Intel i7-2630QM + SanDisk SSD)
+
+![](sample.gif)
 
 ## Usage
 ### simple search in the current directory
@@ -14,3 +17,11 @@ __BUT__: It is _slow_ as it is _simple_. Not recommend to use for fast searching
 
 ### ...and a directory
 `$python sing -p "non-free" -d "/etc/apt/"`
+
+### an advanced example
+`$python sing -p "error" -o "/home/me/matching_lines" -d "/var/log/syslog" -n 8`
+
+## Help is on the way!
+just type `$python sing -h`
+
+![](help_output.png)
